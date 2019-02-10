88294b4b3239c72347b5519e13e2b3a4c6132218
Antoni Kepinski <keppi@o2.pl>
2019-02-10 22:11:24 +0100

    Import modern-normalize and blueprint.css from node_modules instead of jsDelivr

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,9 @@ import App, {Container} from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 
+import '../node_modules/modern-normalize/modern-normalize.css';
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+
 export default class MyApp extends App {
 	static async getInitialProps({Component, ctx}) {
 		let pageProps = {};
