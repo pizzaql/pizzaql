2d7387c49b35d8b8658ed9462b0c66e60d333ca2
Antoni Kepinski <keppi@o2.pl>
2019-02-11 15:54:39 +0100

    typo

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
 					<meta name="og:site_name" content="PizzaQL"/>
 					<meta name="og:type" content="website"/>
 					<link rel="icon" href="static/favicon.png"/>
-					{/* Temporary workaround, since importing blueprint icons with Next.js thrown an error */}
+					{/* Temporary workaround, since importing blueprint icons with Next.js throws an error */}
 					<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@blueprintjs/icons@3.6.0/lib/css/blueprint-icons.css"/>
 					{/* eslint-disable react/no-danger */}
 					<style dangerouslySetInnerHTML={{__html: `
