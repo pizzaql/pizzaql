b29393e570f49cb6baad672519bebe9ec4e53e27
Antoni Kepinski <keppi@o2.pl>
2019-02-17 19:56:47 +0100

    Add more integration tests

---
+++ frontend/cypress/integration/select.spec.js
@@ -0,0 +1,17 @@
+describe('Select', () => {
+	beforeEach(() => {
+		cy.visit('http://localhost:3000');
+	});
+
+	it('Has class bp3-select', () => {
+		cy.get('div').should('have.class', 'bp3-select');
+	});
+
+	it('Has prop name', () => {
+		cy.get('select').should('have.prop', 'name');
+	});
+
+	it('Has option with prop value', () => {
+		cy.get('select').find('option').should('have.prop', 'value');
+	});
+});
