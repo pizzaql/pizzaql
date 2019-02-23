b4662a59ccb7c9afeb7fd6d51a3ed0aa93bc507c
Antoni Kepinski <keppi@o2.pl>
2019-02-23 12:49:04 +0100

    Test thank you page

---
+++ frontend/cypress/integration/order.spec.js
@@ -0,0 +1,29 @@
+describe('Thank you page', () => {
+	beforeEach(() => {
+		cy.visit('http://localhost:3000/order?id="123123123"');
+    });
+    
+    it('Has header with class thanks', () => {
+		cy.get('h1').should('have.class', 'thanks');
+    });
+
+    it('Has div with class centerify', () => {
+		cy.get('div').should('have.class', 'centerify');
+	});
+    
+    it('Has button with class bp3-button', () => {
+		cy.get('button').should('have.class', 'bp3-button');
+	});
+
+    it('Has input with class bp3-input', () => {
+		cy.get('input').should('have.class', 'bp3-input');
+    });
+    
+    it('Has input with prop readonly', () => {
+		cy.get('input').should('have.prop', 'readonly');
+	});
+
+	it('Has input with valid value', () => {
+		cy.get('input').should('have.value', '123123123');
+	});
+});
