e93816d9f6047d355c9fab9cbe08b793ab38304b
Antoni Kepinski <keppi@o2.pl>
2019-02-27 19:18:27 +0100

    Fix SSR for styled-components!

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,11 +3,25 @@ import Document, {Head, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
-	static async getInitialProps({renderPage}) {
+	static async getInitialProps(ctx) {
 		const sheet = new ServerStyleSheet();
-		const page = await renderPage(App => props => sheet.collectStyles(<App {...props}/>));
-		const styleTags = sheet.getStyleElement();
-		return {...page, styleTags};
+		const originalRenderPage = ctx.renderPage;
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: App => props => sheet.collectStyles(<App {...props}/>)
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+
+			return {
+				...initialProps,
+				styles: <>{initialProps.styles}{sheet.getStyleElement()}</>
+			};
+		} finally {
+			sheet.seal();
+		}
 	}
 
 	render() {
