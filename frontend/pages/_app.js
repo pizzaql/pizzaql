3b5e20636abb95e3c8ab5a7bc4682e5e5b8741d6
Antoni Kepinski <keppi@o2.pl>
2019-03-02 16:55:50 +0100

    Fix Apollo error about fetch api

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,10 +2,15 @@ import App, {Container} from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import {ApolloProvider} from 'react-apollo';
+import fetch from 'isomorphic-unfetch';
 import ApolloClient, {InMemoryCache} from 'apollo-boost';
 
 import '../node_modules/modern-normalize/modern-normalize.css';
 import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+
+if (!process.browser) {
+	global.fetch = fetch;
+}
 
 const client = new ApolloClient({
 	uri: 'http://localhost:4466',
