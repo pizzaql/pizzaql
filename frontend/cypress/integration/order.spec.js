569f3ba734111f97ff2ec4ae5ef32511e7418e4e
Antoni Kepinski <keppi@o2.pl>
2019-03-21 20:57:20 +0100

    lint

--- frontend/cypress/integration/order.spec.js
+++ frontend/cypress/integration/order.spec.js
@@ -1,25 +1,25 @@
 describe('Thank you page', () => {
 	before(() => {
 		cy.visit('http://localhost:3000/order?id="123123123"');
-    });
-    
-    it('Has header with class thanks', () => {
-		cy.get('h1').should('have.class', 'thanks');
-    });
+	});
 
-    it('Has div with class centerify', () => {
+	it('Has header with class thanks', () => {
+		cy.get('h1').should('have.class', 'thanks');
+	});
+
+	it('Has div with class centerify', () => {
 		cy.get('div').should('have.class', 'centerify');
 	});
-    
-    it('Has button with class bp3-button', () => {
+
+	it('Has button with class bp3-button', () => {
 		cy.get('button').should('have.class', 'bp3-button');
 	});
 
-    it('Has input with class bp3-input', () => {
+	it('Has input with class bp3-input', () => {
 		cy.get('input').should('have.class', 'bp3-input');
-    });
-    
-    it('Has input with prop readonly', () => {
+	});
+
+	it('Has input with prop readonly', () => {
 		cy.get('input').should('have.prop', 'readonly');
 	});
 
