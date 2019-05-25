7a18bc3790b232241a53a4cce8e460be4d0d878b
Antoni Kepinski <xxczaki@pm.me>
2019-05-25 19:36:20 +0200

    Prevent the user from selecting hours after the restaurant is closed (TODO: global configuration)

--- frontend/components/form/time-select.js
+++ frontend/components/form/time-select.js
@@ -12,11 +12,15 @@ const getTime = (number, isHalfPast) => {
 	return isHalfPast ? `${date}:30` : `${date}:00`;
 };
 
-const Wrapper = ({className}) => (
-	<Label className={className}>
-    Delivery time:
-		<div className="bp3-select">
-			<FastField name="time" component="select" placeholder="Time" required>
+const hoursSelect = () => {
+	// Change to false if you want to display all hours
+	const hoursLock = true;
+
+	const hours = new Date().getHours();
+
+	if (hoursLock && hours === 14) {
+		return (
+			<>
 				<option value="">Select</option>
 				<option value="ASAP">As fast as possible</option>
 				<option>{getTime(3)}</option>
@@ -24,7 +28,65 @@ const Wrapper = ({className}) => (
 				<option>{getTime(4)}</option>
 				<option>{getTime(4, true)}</option>
 				<option>{getTime(5)}</option>
-				<option>{getTime(5, true)}</option>
+			</>
+		);
+	}
+
+	if (hoursLock && hours === 15) {
+		return (
+			<>
+				<option value="">Select</option>
+				<option value="ASAP">As fast as possible</option>
+				<option>{getTime(3)}</option>
+				<option>{getTime(3, true)}</option>
+				<option>{getTime(4)}</option>
+			</>
+		);
+	}
+
+	if (hoursLock && hours === 16) {
+		return (
+			<>
+				<option value="">Select</option>
+				<option value="ASAP">As fast as possible</option>
+				<option>{getTime(3)}</option>
+			</>
+		);
+	}
+
+	if (hoursLock && (hours === 18 || hours === 17)) {
+		return (
+			<>
+				<option value="">Select</option>
+				<option value="ASAP">As fast as possible</option>
+			</>
+		);
+	}
+
+	if (hoursLock && hours <= 19) {
+		return <option disabled value="">You can not order now!</option>;
+	}
+
+	return (
+		<>
+			<option value="">Select</option>
+			<option value="ASAP">As fast as possible</option>
+			<option>{getTime(3)}</option>
+			<option>{getTime(3, true)}</option>
+			<option>{getTime(4)}</option>
+			<option>{getTime(4, true)}</option>
+			<option>{getTime(5)}</option>
+			<option>{getTime(5, true)}</option>
+		</>
+	);
+};
+
+const Wrapper = ({className}) => (
+	<Label className={className}>
+    Delivery time:
+		<div className="bp3-select">
+			<FastField name="time" component="select" placeholder="Time" required>
+				{hoursSelect()}
 			</FastField>
 		</div>
 	</Label>
