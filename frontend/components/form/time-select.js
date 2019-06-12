5fb17971abc4d841284cf8fa14085eb0b48044c4
Antoni Kepinski <xxczaki@pm.me>
2019-06-12 23:17:07 +0200

    Move hours helper to a component

--- frontend/components/form/time-select.js
+++ frontend/components/form/time-select.js
@@ -2,84 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import {FastField} from 'formik';
 import {Label} from '@blueprintjs/core';
-import {format, addHours} from 'date-fns';
 import PropTypes from 'prop-types';
 
-const getTime = (number, isHalfPast) => {
-	const formatted = addHours(new Date(), number);
-	const date = format(formatted, 'HH');
-
-	return isHalfPast ? `${date}:30` : `${date}:00`;
-};
-
-const hoursSelect = () => {
-	// Change to true if you want to enable the hours lock
-	const hoursLock = false;
-
-	const hours = new Date().getHours();
-
-	if (hoursLock && hours === 14) {
-		return (
-			<>
-				<option value="">Select</option>
-				<option value="ASAP">As fast as possible</option>
-				<option>{getTime(3)}</option>
-				<option>{getTime(3, true)}</option>
-				<option>{getTime(4)}</option>
-				<option>{getTime(4, true)}</option>
-				<option>{getTime(5)}</option>
-			</>
-		);
-	}
-
-	if (hoursLock && hours === 15) {
-		return (
-			<>
-				<option value="">Select</option>
-				<option value="ASAP">As fast as possible</option>
-				<option>{getTime(3)}</option>
-				<option>{getTime(3, true)}</option>
-				<option>{getTime(4)}</option>
-			</>
-		);
-	}
-
-	if (hoursLock && hours === 16) {
-		return (
-			<>
-				<option value="">Select</option>
-				<option value="ASAP">As fast as possible</option>
-				<option>{getTime(3)}</option>
-			</>
-		);
-	}
-
-	if (hoursLock && (hours === 18 || hours === 17)) {
-		return (
-			<>
-				<option value="">Select</option>
-				<option value="ASAP">As fast as possible</option>
-			</>
-		);
-	}
-
-	if (hoursLock && hours <= 19) {
-		return <option disabled value="">Ordering disabled</option>;
-	}
-
-	return (
-		<>
-			<option value="">Select</option>
-			<option value="ASAP">As fast as possible</option>
-			<option>{getTime(3)}</option>
-			<option>{getTime(3, true)}</option>
-			<option>{getTime(4)}</option>
-			<option>{getTime(4, true)}</option>
-			<option>{getTime(5)}</option>
-			<option>{getTime(5, true)}</option>
-		</>
-	);
-};
+import hoursSelect from './hours-helper';
 
 const Wrapper = ({className}) => (
 	<Label className={className}>
