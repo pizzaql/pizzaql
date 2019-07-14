fb0d7d46fe9de1d643e66af224f00b5d44fc372c
Antoni Kepinski <xxczaki@pm.me>
2019-07-14 13:33:25 +0200

    Remove react-lazyload

--- frontend/pages/admin.js
+++ frontend/pages/admin.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {Alert, Button, Callout, Icon, Position, Intent, Toaster, Spinner} from '@blueprintjs/core';
 import {Query, Mutation} from 'react-apollo';
-import LazyLoad from 'react-lazyload';
 import secureTemplate from '../static/auth/secure-template';
 
 import Container from '../components/dashboard/container';
@@ -61,133 +60,131 @@ const Secret = () => {
 					return (
 						<Grid>
 							{data.orders.reverse().map(el => (
-								<LazyLoad key={el.id}>
-									<Callout
-										style={{marginBottom: '30px'}}
-										title={`Order id. ${el.id.slice(18)}`}
-										intent={el.status === 'completed' ? 'success' : '' || el.status === 'cancelled' ? 'warning' : ''}
-										icon="flag"
-										key={el.id}
-									>
-										<p>Status: {el.status}</p>
-										<ul>
-											<li>Type: <strong>{el.type}</strong></li>
-											<li>Size: <strong>{el.size}</strong></li>
-											<li>Dough: <strong>{el.dough}</strong></li>
-											<li>Name: <strong>{el.name}</strong></li>
-											<li>Phone: <strong>{el.phone}</strong></li>
-											<li>Street: <strong>{el.street}</strong></li>
-											<li>City: <strong>{el.city}</strong></li>
-											<li>Time: <strong>{el.time}</strong></li>
-										</ul>
-										<ButtonGroup>
-											<Mutation
-												mutation={CHANGE_ORDER_STATUS}
-											>
-												{(updateOrder, {loading, error}) => (
-													<>
-														<Button
-															style={{margin: '5px'}}
-															icon="tick"
-															intent="primary"
-															loading={loading}
-															disabled={el.status === 'completed'}
-															key={el.id}
-															data-order-id={el.id}
-															onClick={e => {
-																const orderID = e.currentTarget.attributes['data-order-id'].value;
-																updateOrder({variables: {status: 'completed', id: orderID}});
-																showToaster('Status changed!', error);
-															}}
-														>
+								<Callout
+									style={{marginBottom: '30px'}}
+									title={`Order id. ${el.id.slice(18)}`}
+									intent={el.status === 'completed' ? 'success' : 'none' || el.status === 'cancelled' ? 'warning' : 'none'}
+									icon="flag"
+									key={el.id}
+								>
+									<p>Status: {el.status}</p>
+									<ul>
+										<li>Type: <strong>{el.type}</strong></li>
+										<li>Size: <strong>{el.size}</strong></li>
+										<li>Dough: <strong>{el.dough}</strong></li>
+										<li>Name: <strong>{el.name}</strong></li>
+										<li>Phone: <strong>{el.phone}</strong></li>
+										<li>Street: <strong>{el.street}</strong></li>
+										<li>City: <strong>{el.city}</strong></li>
+										<li>Time: <strong>{el.time}</strong></li>
+									</ul>
+									<ButtonGroup>
+										<Mutation
+											mutation={CHANGE_ORDER_STATUS}
+										>
+											{(updateOrder, {loading, error}) => (
+												<>
+													<Button
+														style={{margin: '5px'}}
+														icon="tick"
+														intent="primary"
+														loading={loading}
+														disabled={el.status === 'completed'}
+														key={el.id}
+														data-order-id={el.id}
+														onClick={e => {
+															const orderID = e.currentTarget.attributes['data-order-id'].value;
+															updateOrder({variables: {status: 'completed', id: orderID}});
+															showToaster('Status changed!', error);
+														}}
+													>
 												Complete
-														</Button>
-														{error && <p>Error :( Please try again</p>}
-													</>
-												)}
-											</Mutation>
-											<Mutation
-												mutation={CHANGE_ORDER_STATUS}
-											>
-												{(updateOrder, {loading, error}) => (
-													<>
-														<Button
-															style={{margin: '5px'}}
-															icon="cross"
-															intent="warning"
-															loading={loading}
-															disabled={el.status === 'cancelled'}
-															key={el.id}
-															data-order-id={el.id}
-															onClick={e => {
-																const orderID = e.currentTarget.attributes['data-order-id'].value;
-																updateOrder({variables: {status: 'cancelled', id: orderID}});
-																showToaster('Status changed!', error);
-															}}
-														>
+													</Button>
+													{error && <p>Error :( Please try again</p>}
+												</>
+											)}
+										</Mutation>
+										<Mutation
+											mutation={CHANGE_ORDER_STATUS}
+										>
+											{(updateOrder, {loading, error}) => (
+												<>
+													<Button
+														style={{margin: '5px'}}
+														icon="cross"
+														intent="warning"
+														loading={loading}
+														disabled={el.status === 'cancelled'}
+														key={el.id}
+														data-order-id={el.id}
+														onClick={e => {
+															const orderID = e.currentTarget.attributes['data-order-id'].value;
+															updateOrder({variables: {status: 'cancelled', id: orderID}});
+															showToaster('Status changed!', error);
+														}}
+													>
 												Cancel
-														</Button>
-														{error && <p>Error :( Please try again</p>}
-													</>
-												)}
-											</Mutation>
-											<Mutation
-												mutation={DELETE_ORDER}
-												update={(cache, {data: {deleteOrder}}) => {
-													const {orders} = cache.readQuery({query: GET_ORDERS});
+													</Button>
+													{error && <p>Error :( Please try again</p>}
+												</>
+											)}
+										</Mutation>
+										<Mutation
+											mutation={DELETE_ORDER}
+											update={(cache, {data: {deleteOrder}}) => {
+												const {orders} = cache.readQuery({query: GET_ORDERS});
 
-													const result = orders.reverse().filter(el => (deleteOrder.id.indexOf(el.id) === -1));
+												const result = orders.reverse().filter(el => (deleteOrder.id.indexOf(el.id) === -1));
 
-													cache.writeQuery({
-														query: GET_ORDERS,
-														data: {orders: result}
-													});
-												}}
-											>
-												{(deleteOrder, {loading, error}) => (
-													<>
-														<Button
-															style={{margin: '5px'}}
-															icon="trash"
-															intent="danger"
-															loading={loading}
-															key={el.id}
-															data-order-id={el.id}
-															onClick={e => {
-																setId(e.currentTarget.attributes['data-order-id'].value);
-																setIsOpen(true);
-															}}
-														>
+												cache.writeQuery({
+													query: GET_ORDERS,
+													data: {orders: result}
+												});
+											}}
+										>
+											{(deleteOrder, {loading, error}) => (
+												<>
+													<Button
+														style={{margin: '5px'}}
+														icon="trash"
+														intent="danger"
+														loading={loading}
+														key={el.id}
+														data-order-id={el.id}
+														onClick={e => {
+															setId(e.currentTarget.attributes['data-order-id'].value);
+															setIsOpen(true);
+														}}
+													>
 												Delete
-														</Button>
-														<Alert
-															cancelButtonText="Cancel"
-															confirmButtonText="Delete"
-															icon="trash"
-															intent={Intent.DANGER}
-															isOpen={isOpen}
-															onCancel={() => {
-																setIsOpen(false);
-															}}
-															data-order-id={el.id}
-															onConfirm={async () => {
-																await deleteOrder({variables: {id}});
-																await setIsOpen(false);
-																await setId('');
-																showToaster('Order deleted!', error);
-															}}
-														>
-															<p>
+													</Button>
+													<Alert
+														cancelButtonText="Cancel"
+														confirmButtonText="Delete"
+														icon="trash"
+														intent={Intent.DANGER}
+														isOpen={isOpen}
+														onCancel={() => {
+															setIsOpen(false);
+														}}
+														data-order-id={el.id}
+														onConfirm={async () => {
+															await deleteOrder({variables: {id}});
+															await setIsOpen(false);
+															await setId('');
+															showToaster('Order deleted!', error);
+														}}
+													>
+														<p>
                         Are you sure you want delete this order? You will not be able to restore it later.
-															</p>
-														</Alert>
-														{error && <p>Error :( Please try again</p>}
-													</>
-												)}
-											</Mutation>
-										</ButtonGroup>
-									</Callout>
-								</LazyLoad>
+														</p>
+													</Alert>
+													{error && <p>Error :( Please try again</p>}
+												</>
+											)}
+										</Mutation>
+									</ButtonGroup>
+								</Callout>
 							))}
 						</Grid>
 					);
