cb1a5d81804c6bc7e0f904ef2329371f010c2d43
Antoni Kepinski <xxczaki@pm.me>
2019-07-24 00:14:18 +0200

    Show time left to order delivery :alarm_clock:

--- frontend/pages/order.js
+++ frontend/pages/order.js
@@ -2,9 +2,12 @@ import React, {useState, useRef} from 'react';
 import {withRouter} from 'next/router';
 import Link from 'next/link';
 import {createGlobalStyle} from 'styled-components';
-import {Button, Card, Elevation} from '@blueprintjs/core';
+import {Button, Card, Elevation, Spinner} from '@blueprintjs/core';
+import {Query} from 'react-apollo';
 
 import Container from '../components/form/container';
+
+import {GET_ORDER_BY_ID} from '../components/api';
 
 const GlobalStyle = createGlobalStyle`
 	body {
@@ -16,7 +19,12 @@ const Order = ({router: {query}}) => {
 	const [copySuccess, setCopySuccess] = useState('');
 	const inputEl = useRef(null);
 
-	const id = query.id.replace(/"/g, '');
+	if (!query.id) {
+		return <p>Order id not provided</p>;
+	}
+
+	const shortId = query.id.replace(/"/g, '').slice(18);
+	const {id} = query;
 
 	const copyToClipboard = () => {
 		inputEl.current.select();
@@ -28,22 +36,40 @@ const Order = ({router: {query}}) => {
 	return (
 		<Container style={{textAlign: 'center'}}>
 			<Card elevation={Elevation.FOUR}>
-				<h1 style={{fontSize: '3rem'}} className="thanks">Thank you!</h1>
-				<br/>
-				<p>Your order number is:</p>
-				<br/>
-				<div style={{width: '12em', margin: 'auto'}} className="bp3-input-group">
-					<input ref={inputEl} className="bp3-input" value={id} readOnly/>
-					<Button className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-clipboard" onClick={copyToClipboard}/>
-					{copySuccess}
-				</div>
-				<br/>
-				<br/>
-				<p>If you won&apos;t receive your order after the selected time, please call us: <strong>234 567 890</strong></p>
-				<br/>
-				<Link prefetch href="/">
-					<Button>Order another pizza!</Button>
-				</Link>
+				<Query query={GET_ORDER_BY_ID} variables={{id}}>
+					{({loading, error, data}) => {
+						if (loading) {
+							return <Spinner/>;
+						}
+
+						if (error) {
+							return <p>Order not found or it&apos;s id is invalid</p>;
+						}
+
+						return (
+							<>
+								<h1 style={{fontSize: '3rem'}} className="thanks">Thank you!</h1>
+								<br/>
+								<p>Your order number is:</p>
+								<br/>
+								<div style={{width: '12em', margin: 'auto'}} className="bp3-input-group">
+									<input ref={inputEl} className="bp3-input" value={shortId} readOnly/>
+									<Button className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-clipboard" onClick={copyToClipboard}/>
+									{copySuccess}
+								</div>
+								<br/>
+								<h4 style={{fontSize: '1.2rem'}}>You will receive your order {data.order.time === 'ASAP' ? 'in about an hour' : `at ${data.order.time}`}</h4>
+								<br/>
+								<br/>
+								<p>If you won&apos;t receive your order after that time, please call us: <strong>234 567 890</strong></p>
+								<br/>
+								<Link prefetch href="/">
+									<Button>Order another pizza!</Button>
+								</Link>
+							</>
+						);
+					}}
+				</Query>
 			</Card>
 			<GlobalStyle/>
 		</Container>
