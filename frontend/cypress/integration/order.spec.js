2ce52d4d286d5fe217123383cd7829e753750a98
Antoni Kepinski <xxczaki@pm.me>
2019-07-24 11:46:46 +0200

    Update order tests

--- frontend/cypress/integration/order.spec.js
+++ frontend/cypress/integration/order.spec.js
@@ -3,23 +3,7 @@ describe('Thank you page', () => {
 		cy.visit('http://localhost:3000/order?id="123123123"');
 	});
 
-	it('Has header with class thanks', () => {
-		cy.get('h1').should('have.class', 'thanks');
-	});
-
-	it('Has button with class bp3-button', () => {
-		cy.get('button').should('have.class', 'bp3-button');
-	});
-
-	it('Has input with class bp3-input', () => {
-		cy.get('input').should('have.class', 'bp3-input');
-	});
-
-	it('Has input with prop readonly', () => {
-		cy.get('input').should('have.prop', 'readonly');
-	});
-
-	it('Has input with valid value', () => {
-		cy.get('input').should('have.value', '123123123');
+	it('Has error message', () => {
+		cy.get('p').should('contain', `Order not found or it's id is invalid`);
 	});
 });
