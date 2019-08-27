1855574d9d53cb442cabd6dff5a1d804917aaccb
Antoni Kepinski <xxczaki@pm.me>
2019-08-27 21:02:25 +0200

    Rewrite thank you page to @apollo/react-hooks :tada:

--- frontend/pages/order.js
+++ frontend/pages/order.js
@@ -3,7 +3,7 @@ import {withRouter} from 'next/router';
 import Link from 'next/link';
 import {createGlobalStyle} from 'styled-components';
 import {Button, Card, Elevation, Spinner} from '@blueprintjs/core';
-import {Query} from 'react-apollo';
+import {useQuery} from '@apollo/react-hooks';
 
 import Container from '../components/form/container';
 
@@ -19,12 +19,15 @@ const Order = ({router: {query}}) => {
 	const [copySuccess, setCopySuccess] = useState('');
 	const inputEl = useRef(null);
 
-	if (!query.id) {
-		return <p>Order id not provided</p>;
-	}
-
-	const shortId = query.id.replace(/"/g, '').slice(18);
 	const {id} = query;
+
+	const {loading, error, data} = useQuery(GET_ORDER_BY_ID, {
+		variables: {id}
+	});
+
+	if (error || !query.id) {
+		return <p>Order not found or it&apos;s id is invalid</p>;
+	}
 
 	const copyToClipboard = () => {
 		inputEl.current.select();
@@ -33,43 +36,32 @@ const Order = ({router: {query}}) => {
 		setCopySuccess('Copied!');
 	};
 
+	const shortId = query.id.replace(/"/g, '').slice(18);
+
 	return (
 		<Container style={{textAlign: 'center'}}>
 			<Card elevation={Elevation.FOUR}>
-				<Query query={GET_ORDER_BY_ID} variables={{id}}>
-					{({loading, error, data}) => {
-						if (loading) {
-							return <Spinner/>;
-						}
-
-						if (error) {
-							return <p>Order not found or it&apos;s id is invalid</p>;
-						}
-
-						return (
-							<>
-								<h1 style={{fontSize: '3rem'}} className="thanks">Thank you!</h1>
-								<br/>
-								<p>Your order number is:</p>
-								<br/>
-								<div style={{width: '12em', margin: 'auto'}} className="bp3-input-group">
-									<input ref={inputEl} className="bp3-input" value={shortId} readOnly/>
-									<Button className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-clipboard" onClick={copyToClipboard}/>
-									{copySuccess}
-								</div>
-								<br/>
-								<h4 style={{fontSize: '1.2rem'}}>You will receive your order {data.order.time === 'ASAP' ? 'in about an hour' : `at ${data.order.time}`}</h4>
-								<br/>
-								<br/>
-								<p>If you won&apos;t receive your order after that time, please call us: <strong>234 567 890</strong></p>
-								<br/>
-								<Link prefetch href="/">
-									<Button>Order another pizza!</Button>
-								</Link>
-							</>
-						);
-					}}
-				</Query>
+				{loading ? <Spinner/> :
+				<>
+					<h1 style={{fontSize: '3rem'}} className="thanks">Thank you!</h1>
+					<br/>
+					<p>Your order number is:</p>
+					<br/>
+					<div style={{width: '12em', margin: 'auto'}} className="bp3-input-group">
+						<input ref={inputEl} className="bp3-input" value={shortId} readOnly/>
+						<Button className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-clipboard" onClick={copyToClipboard}/>
+						{copySuccess}
+					</div>
+					<br/>
+					<h4 style={{fontSize: '1.2rem'}}>You will receive your order {data.order.time === 'ASAP' ? 'in about an hour' : `at ${data.order.time}`}</h4>
+					<br/>
+					<br/>
+					<p>If you won&apos;t receive your order after that time, please call us: <strong>234 567 890</strong></p>
+					<br/>
+					<Link href="/">
+						<Button>Order another pizza!</Button>
+					</Link>
+				</>}
 			</Card>
 			<GlobalStyle/>
 		</Container>
