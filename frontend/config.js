320d6ae6cc16aac46f691319aeba2100e4466a9e
Antoni Kepinski <xxczaki@pm.me>
2019-09-01 21:08:35 +0200

    Implement global config file

---
+++ frontend/config.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const config = {
+	auth0: {
+		clientID: 'BUUYjCiP2SYKQBB48OZRALsccmijoubx',
+		domain: 'pizzaql.eu.auth0.com'
+	},
+	stripe: {
+		key: 'pk_test_A6mUVOGtiDJwvnJsg1AmoNxO'
+	},
+	restaurant: {
+		name: 'PizzaQL',
+		phone: 693827127,
+		hoursLock: false
+	},
+	prices: {
+		Margharita: {
+			Small: 17,
+			Medium: 20,
+			Large: 23
+		},
+		Funghi: {
+			Small: 20,
+			Medium: 23,
+			Large: 26
+		},
+		Cacciatore: {
+			Small: 16,
+			Medium: 19,
+			Large: 22
+		},
+		Vesuvio: {
+			Small: 19,
+			Medium: 22,
+			Large: 25
+		},
+		Milano: {
+			Small: 20,
+			Medium: 23,
+			Large: 26
+		},
+		Capriciosa: {
+			Small: 20,
+			Medium: 23,
+			Large: 26
+		},
+		Prosciutto: {
+			Small: 21,
+			Medium: 24,
+			Large: 27
+		},
+		Hawaiano: {
+			Small: 21,
+			Medium: 24,
+			Large: 27
+		},
+		Rimini: {
+			Small: 20,
+			Medium: 23,
+			Large: 26
+		},
+		Bali: {
+			Small: 22,
+			Medium: 25,
+			Large: 28
+		},
+		Pepperoni: {
+			Small: 20,
+			Medium: 23,
+			Large: 26
+		},
+		Torino: {
+			Small: 20,
+			Medium: 23,
+			Large: 26
+		},
+		thickDough: 3
+	}
+};
+
+export default config;
