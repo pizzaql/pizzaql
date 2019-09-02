619e32355b1ec8a72e61e98f306350badba70e1a
Antoni Kepinski <xxczaki@pm.me>
2019-09-02 22:02:29 +0200

    Use next-offline

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,28 @@
+const withCSS = require('@zeit/next-css');
 const withFonts = require('next-fonts');
-const withCss = require('@zeit/next-css');
+const withOffline = require('next-offline');
 
-module.exports = withCss(withFonts());
+const nextConfig = {
+	workboxOpts: {
+		swDest: 'static/service-worker.js',
+		runtimeCaching: [
+			{
+				urlPattern: /^https?.*/,
+				handler: 'networkFirst',
+				options: {
+					cacheName: 'https-calls',
+					networkTimeoutSeconds: 15,
+					expiration: {
+						maxEntries: 150,
+						maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+					},
+					cacheableResponse: {
+						statuses: [0, 200]
+					}
+				}
+			}
+		]
+	}
+};
+
+module.exports = withCSS(withFonts(withOffline(nextConfig)));
