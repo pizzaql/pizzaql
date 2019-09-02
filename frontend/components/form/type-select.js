36ec2e595a19d5b6b34c57c29c17b8b052ee8449
Antoni Kepinski <xxczaki@pm.me>
2019-09-02 18:24:07 +0200

    Update type-select component to show pizzas, that are in config file

--- frontend/components/form/type-select.js
+++ frontend/components/form/type-select.js
@@ -4,24 +4,19 @@ import {FastField} from 'formik';
 import {Label} from '@blueprintjs/core';
 import PropTypes from 'prop-types';
 
+import config from '../../config';
+
+const {pizzas} = config;
+
+const types = Object.keys(pizzas);
+
 const Wrapper = ({className}) => (
 	<Label className={className}>
 Pizza Type:
 		<div className="bp3-select">
 			<FastField name="type" component="select" placeholder="Pizza Type" required>
 				<option value="">Select</option>
-				<option value="Margharita">Margharita</option>
-				<option value="Funghi">Funghi</option>
-				<option value="Cacciatore">Cacciatore</option>
-				<option value="Vesuvio">Vesuvio</option>
-				<option value="Milano">Milano</option>
-				<option value="Capriciosa">Capriciosa</option>
-				<option value="Prosciutto">Prosciutto</option>
-				<option value="Hawaiano">Hawaiano</option>
-				<option value="Rimini">Rimini</option>
-				<option value="Bali">Bali</option>
-				<option value="Pepperoni">Pepperoni</option>
-				<option value="Torino">Torino</option>
+				{types.map(type => <option value={type} key={type}>{type}</option>)}
 			</FastField>
 		</div>
 	</Label>
