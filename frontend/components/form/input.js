3944763198c6d1b7dcb9d20fdaf685f0bb8bf1ef
Antoni Kepinski <xxczaki@pm.me>
2019-11-02 17:25:05 +0100

    Simplify the Input component

--- frontend/components/form/input.js
+++ frontend/components/form/input.js
@@ -1,31 +1,8 @@
-import React from 'react';
 import styled from 'styled-components';
-import {ErrorMessage, FastField} from 'formik';
-import {Label} from '@blueprintjs/core';
-import PropTypes from 'prop-types';
 
-const Wrapper = ({className, label, type, name, placeholder, required}) => (
-	<Label className={className}>
-		{label}
-		<FastField className="bp3-input" type={type} name={name} placeholder={placeholder} required={required}/>
-		<ErrorMessage name={name} component="div"/>
-	</Label>
-);
-
-const Input = styled(Wrapper)`
-    .bp3-input {
-        width: 100%;
-		user-select: none;
-    }
+const Input = styled.input`
+    width: 100%;
+	user-select: none;
 `;
-
-Input.propTypes = {
-	className: PropTypes.string,
-	label: PropTypes.string,
-	type: PropTypes.string,
-	name: PropTypes.string,
-	placeholder: PropTypes.string,
-	required: PropTypes.bool.isRequired
-};
 
 export default Input;
